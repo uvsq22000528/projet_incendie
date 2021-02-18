50f5d1d87c0ee48b1a3e972c8447ab41d076b841
uvsq22000528 <72202495+uvsq22000528@users.noreply.github.com>
2021-02-18 09:56:51 +0100

    Create incendie.py

---
+++ incendie.py
@@ -0,0 +1,7 @@
+#########################################
+# groupe MPCI 5
+# Anthonin Le nevez
+# Titi LEVAINQUEUR
+# etc...
+# https://github.com/uvsq-info/l1-python
+#########################################
