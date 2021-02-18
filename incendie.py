d8bcf036b4b3e2bbdfdf8cd99afd7409396948d8
uvsq22000528 <72202495+uvsq22000528@users.noreply.github.com>
2021-02-18 10:02:13 +0100

    Update incendie.py

--- incendie.py
+++ incendie.py
@@ -1,7 +1,9 @@
 #########################################
 # groupe MPCI 5
 # Anthonin Le nevez
-# Titi LEVAINQUEUR
-# etc...
+# Kaïs Cheboub
+# Anissa Dialo
+# Enzo Reale
+# Phkar Romdoul
 # https://github.com/uvsq-info/l1-python
 #########################################
