6cb89b23a2e96efb6f10e032411e93e3fd7ec12d
Enzo REALE <enzo.reale@ens.uvsq.fr>
2021-02-19 05:23:33 +0100

    enter

--- incendie.py
+++ incendie.py
@@ -11,12 +11,84 @@
 
 #importation
 import tkinter as tk
+import random, marshal
+
 
 #creation de la fenetre
-racine = tk.TK()
+racine = tk.Tk()
 racine.title("simulation incendie")
 racine.resizable(height = 300, width = 300)
 
-#Couleur propagation
-Couleur=[Bleu, Vert, Rouge, Jaune, Gris, Noir]
 
+#Couleur
+Bleu = "blue"
+Vert = "green4"
+Rouge = "red"
+Jaune =  "yellow"
+Gris = "grey30"
+Noir = "grey1"
+
+#Affichage
+HEIGHT, WIDTH = 800, 800
+haut_case= HEIGHT//80
+larg_case = WIDTH//80
+canvas = tk.Canvas(racine, bg="white", height=800, width=800)
+canvas.grid(column=0, rowspan=5)
+
+
+#variable
+Couleur=[Bleu, Vert, Rouge, Jaune, Gris, Noir]
+Largeur, Hauteur = 100, 100
+Terrain = Hauteur*[Largeur*[0]]
+
+#constante
+durée_feu = 3
+durée_cendre = 2
+
+
+
+
+#fonction
+
+def aleatzone():
+    global Couleur, larg_case, haut_case, Terrain
+    for i in range (80):
+        for j in range (80):
+            color = random.choice([Bleu, Vert, Jaune])
+            Terrain[i][j] = color
+            canvas.create_rectangle((i*larg_case, j*haut_case),
+                ((i+1)*larg_case, (j+1)*haut_case), fill=color)
+
+def save():
+    global Terrain
+    with open("sauvegarde.txt", 'w')
+    for case in Terrain :
+        open("sauvegarde.txt", 'w') ## Sauvegarde du dictionnaire
+    
+
+
+#Commande
+Createur = tk.Button(racine, width= 10, highlightbackground="#393B3B",text="Crée", font = ("helvetica", "30"), command= aleatzone)
+    # un bouton qui génère un terrain au hasard avec des parcelles d’eau, de forêt et de prairie;
+Save = tk.Button(racine, width= 10, highlightbackground="#393B3B",text="Save", font = ("helvetica", "30"), command= save) 
+    # un bouton pour sauvegarder l’état du terrain dans un fichier;
+Load = tk.Button(racine, width= 10, highlightbackground="#393B3B",text="Load", font = ("helvetica", "30"), command= lambda: caractere("3")) 
+    # un bouton pour charger un terrain depuis un fichier;
+EtapeSuivante = tk.Button(racine, width= 10, highlightbackground="#393B3B",text=">", font = ("helvetica", "30"), command= lambda: caractere("4")) 
+    # un bouton permet d’effectuer une étape de simulation;
+Start = tk.Button(racine, width= 10, highlightbackground="#393B3B",text="Start", font = ("helvetica", "30"), command= lambda: caractere("5")) 
+    # un bouton qui permet de démarrer une simulation;
+Stop = tk.Button(racine, width= 10, highlightbackground="#393B3B",text="Stop", font = ("helvetica", "30"), command= lambda: caractere("6")) 
+    # un bouton pour arrêter la simulation;
+Createur.grid(row= 0, column= 1) # positionnement du Createur
+Save.grid(row= 1, column= 1) # positionnement du Save
+Load.grid(row= 2, column= 1) # positionnement du Load
+EtapeSuivante.grid(row= 3, column= 1) # positionnement de l'Etape suivante
+Start.grid(row= 4, column= 1) # positionnement du Start
+Stop.grid(row= 5, column= 1) # positionnement du Stop
+
+
+
+
+racine.mainloop()
+ 
